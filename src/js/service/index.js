15faeb0dfb9076c972ee40a5ddfb75cf3b0868f4
Ana Pacheco <kornetera@gmail.com>
2023-03-20 19:37:58 +0000

    bueno basta

--- src/js/service/index.js
+++ src/js/service/index.js
@@ -19,10 +19,44 @@ export const addContact = async (contact) => {
       method: "POST",
       body: JSON.stringify(contact),
       headers: {"Content-Type": "application/json"},
-      redirect: 'follow'
+      redirect: "follow",
     });
 
     console.log("POST hecho con exito"); //mensaje en consola para saber que se ha realizado con éxito
+  } catch (error) {
+    console.log("Error:", error); //mensaje de error si el contacto no se carga
+  }
+};
+
+export const modifyContact = async (contact, id) => {
+  const URL_Put = "https://assets.breatheco.de/apis/fake/contact/";
+
+  try {
+    await fetch(`${URL_Put}/${id}`.trim(), {
+      //se envian los datos con post, opciones dadas por la API
+      method: "PUT",
+      body: JSON.stringify(contact),
+      headers: {"Content-Type": "application/json"},
+      redirect: "follow",
+    });
+
+    console.log("PUT hecho con exito"); //mensaje en consola para saber que se ha realizado con éxito
+  } catch (error) {
+    console.log("Error:", error); //mensaje de error si el contacto no se carga
+  }
+};
+
+export const deleteContact = async (id) => {
+  const URL_Del = "https://assets.breatheco.de/apis/fake/contact/";
+
+  try {
+    await fetch(`${URL_Del}/${id}`.trim(), {
+      //se envian los datos con post, opciones dadas por la API
+      method: "DELETE",
+      redirect: "follow",
+    });
+
+    console.log("Delete hecho con exito"); //mensaje en consola para saber que se ha realizado con éxito
   } catch (error) {
     console.log("Error:", error); //mensaje de error si el contacto no se carga
   }
