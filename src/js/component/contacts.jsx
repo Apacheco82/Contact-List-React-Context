845d0a4493f9a86ef8ae107bcf27c906d1640e1d
Ana Pacheco <kornetera@gmail.com>
2023-03-21 16:17:04 +0000

    changes in css

--- src/js/component/contacts.jsx
+++ src/js/component/contacts.jsx
@@ -17,70 +17,80 @@ export const Contacts = (props) => {
   // onClick={handleDelete}
 
   return (
-    <div className="contacts">
-      <div className="contact-info">
-        <div>
-          <h4>{props.full_name}</h4>
-        </div>
-        <div className="icons-container">
-          <i
-            className="fa-solid fa-xmark"
-            data-bs-toggle="modal"
-            data-bs-target="#exampleModal"
-          >
-            <div
-              class="modal fade"
-              id="exampleModal"
-              tabindex="-1"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
+    <>
+      <div className="contacts">
+        <img src="https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2021/05/07/16203887297193.jpg" id="avatar" />
+        <div className="contact-info">
+          <div>
+            <h4>{props.full_name}</h4>
+          </div>
+          <div className="icons-container">
+            <i
+              className="fa-solid fa-xmark"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
             >
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">
-                      Are you sure?
-                    </h1>
-                    <button
-                      type="button"
-                      class="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                    ></button>
-                  </div>
-                  <div class="modal-body">...</div>
-                  <div class="modal-footer">
-                    <button
-                      type="button"
-                      class="btn btn-secondary"
-                      data-bs-dismiss="modal"
-                    >
-                      Cancel
-                    </button>
-                    <button type="button" class="btn btn-primary" onClick={handleDelete}>
-                      Delete
-                    </button>
+              <div
+                className="modal fade"
+                id="exampleModal"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h1 className="modal-title fs-5" id="exampleModalLabel">
+                        Are you sure?
+                      </h1>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">
+                      This contact will be deleted permanently
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-success"
+                        data-bs-dismiss="modal"
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        id="deletebtn"
+                        onClick={handleDelete}
+                      >
+                        Delete
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </i>
-          <Link to={`/editcontact/${props.id}`}>
-            {" "}
-            <i className="fa-solid fa-pen"></i>
-          </Link>
+            </i>
+            <Link to={`/editcontact/${props.id}`}>
+              {" "}
+              <i className="fa-solid fa-pen"></i>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <i className="fa-solid fa-envelope"></i> {props.email}{" "}
+        </div>
+        <div>
+          {" "}
+          <i className="fa-solid fa-location-dot"></i> {props.address}
+        </div>
+        <div>
+          <i className="fa-solid fa-phone"></i> {props.phone}
         </div>
       </div>
-      <div>
-        <i className="fa-solid fa-envelope"></i> {props.email}{" "}
-      </div>
-      <div>
-        {" "}
-        <i className="fa-solid fa-location-dot"></i> {props.address}
-      </div>
-      <div>
-        <i className="fa-solid fa-phone"></i> {props.phone}
-      </div>
-    </div>
+    </>
   );
 };
