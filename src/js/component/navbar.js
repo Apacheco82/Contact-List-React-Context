ebe2dbd6563a7136e9d6e8474582e16be68a68c0
Ana Pacheco <kornetera@gmail.com>
2023-03-21 16:19:25 +0000

    change the navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light mb-3" id="navigate">
+		<nav className="navbar sticky-top" id="navigate">
 		<Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
 		  <div><h2>Contact List</h2></div>
 		</Link>
