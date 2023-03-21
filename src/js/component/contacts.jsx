26be7abb6d0b253a0ffc941811a06c09f9d29dea
Ana Pacheco <kornetera@gmail.com>
2023-03-21 21:51:48 +0000

    arreglado el funcionamiento de borrado con modal

--- src/js/component/contacts.jsx
+++ src/js/component/contacts.jsx
@@ -1,23 +1,34 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {Context} from "../store/appContext";
 import {Link} from "react-router-dom";
 
 export const Contacts = (props) => {
   const {store, actions} = useContext(Context); //contexto global
 
+  const handleId = () => {
+    //con esta función nos aseguramos de almacenar el props.id en una store global de Flux, para no perderlo
+    actions.saveId(props.id); //llamamos a una función de Flux que nos guarda y conserva el props.id del elemento que tengo seleccionado al hacer click en el <i>
+  };
+
   const handleDelete = () => {
     //funcion  intermedia para llamar a la funcion de flux
-    actions.deleteSingleContact(props.id); //la funcion de flux para borrar pasandole el id
+    actions.deleteSingleContact(store.id); //la funcion de flux para borrar pasandole el id, en vez de pasarle el props.id le pasamos el id que tenemos almacenado en Flux, store.id
+    actions.saveId(""); //seteamos de nuevo el store.id a VACÍO para que se quede preparado para la siguiente acción de borrado
   };
 
   /*comentario sobre link: creamos la ruta a la que tenemos que enlazar igual que está definida en layout.js
   en este caso estamos pasando como parametro el id del contacto  */
 
-
   return (
     <>
       <div className="contacts">
-       <div className="avatar"> <img src="https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2021/05/07/16203887297193.jpg" id="avatar" /></div>
+        <div className="avatar">
+          {" "}
+          <img
+            src="https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2021/05/07/16203887297193.jpg"
+            id="avatar"
+          />
+        </div>
         <div className="contact-info">
           <div>
             <h4>{props.full_name}</h4>
@@ -25,6 +36,7 @@ export const Contacts = (props) => {
           <div className="icons-container">
             <i
               className="fa-solid fa-xmark"
+              onClick={handleId}
               data-bs-toggle="modal"
               data-bs-target="#exampleModal"
             >
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -4,6 +4,7 @@ const getState = ({getStore, getActions, setStore}) => {
   return {
     store: {
       contactList: [], //el array que contiene los contactos
+      id: "" //dato global para almacenar props.id y no perderlo al abrir el modal de borrado de contactos
     },
     actions: {
       addContactList: (info) => { //funcion para añadir contactos desde la api al array de store
@@ -18,6 +19,10 @@ const getState = ({getStore, getActions, setStore}) => {
       deleteSingleContact: (id) => { //funcion para borrar un contacto por id DELETE
         deleteContact(id);//se llama a una funcion en index.js
       },
+      saveId: (id) => { //función que guarda el id del contacto que tengo seleccionado al intentar borrarlo
+        setStore({id: id}) //seteamos el valor del store.id con el props.id del contacto que estoy intentando borrar
+
+      }
     },
   };
 };
