a6218cf4cd05dbc7affed9c49f6a54400938f1d1
Ana Pacheco <kornetera@gmail.com>
2023-03-21 13:54:49 +0000

    alert al añadir contacto

--- src/js/views/formulario.jsx
+++ src/js/views/formulario.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useContext} from "react";
 import {Context} from "../store/appContext";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const initialState = {
   //se crea aquí el objeto vacío porque solo lo vamos a usar en form para decirle que se vaya siempre a la agenda "apacheco"
@@ -12,10 +12,9 @@ const initialState = {
 };
 
 export const Formulario = () => {
-
   const {store, actions} = useContext(Context); //contexto global
   const [contact, setContact] = useState(initialState); //el obj contact que viene de useState, se inicializa desde initialState
-  const [alert, setAlert] = useState(false) //para mostrar un alert en pantalla
+  const [load, setLoad] = useState(false); //para mostrar un alert al cargar, como si fuera un spinner
 
   const handleChange = (e) => {
     const value = e.target.value; //se obtiene el valor del input
@@ -25,38 +24,72 @@ export const Formulario = () => {
 
   const handleData = (e) => {
     e.preventDefault(); //para que no recargue la web al darle al boton
-   // console.log(contact);
-    actions.addSingleContact(contact) //llamamos al flux pasando el contacto relleno y desde ahi irá a index.js a hacer Post
-    setContact(initialState) //para vaciar los inputs
-    setAlert(true); //para mostrar el alert
+    // console.log(contact);
+    actions.addSingleContact(contact); //llamamos al flux pasando el contacto relleno y desde ahi irá a index.js a hacer Post
+    setLoad(true); //para mostrar el alert
+    setContact(initialState); //para vaciar los inputs
+    setTimeout(() => {
+      //para mostrar el alert dos segundos antes de quitarlo
+      setLoad(false);
+    }, 2000);
   };
 
-  setTimeout(() => {
-    setAlert(false);
-  }, 5000);// el tiempo que va a estar el alert en pantalla
-;
-
-return (
-  <div className="container container-fluid">
-    {alert ? (
-      <div className="alert alert-success alert-dismissible fade show" role="alert">
-        New contact added successfully!
-      </div>
-    ) : null}
-    <form onChange={handleChange} onSubmit={handleData}>
-      <h3>Add A New Contact</h3>
-      <input type="text" name="full_name" value={contact.full_name} placeholder="Name"></input>
-      <input type="email" name="email" value={contact.email} placeholder="eMail"></input>
-      <input type="text" name="address" value={contact.address} placeholder="Address"></input>
-      <input type="text" name="phone" value={contact.phone} placeholder="Phone Number"></input>
-      <input type="submit" className="btn btn-success" value="Add New Contact"></input>
-    </form>
-    <Link to="/">
-      <button className="btn btn-success" role="button" id="home">
-        Back to Home
-      </button>
-    </Link>
-  </div>
-);
+  return (
+    <div className="container container-fluid">
+      {load ? (
+        <div
+          className="alert alert-success alert-dismissible fade show"
+          role="alert"
+        >
+          New contact added successfully!
+        </div>
+      ) : (
+        <>
+          {" "}
+          <form onChange={handleChange} onSubmit={handleData}>
+            <h3>Add A New Contact</h3>
+            <input
+              type="text"
+              name="full_name"
+              value={contact.full_name}
+              placeholder="Name"
+              required
+            ></input>
+            <input
+              type="email"
+              name="email"
+              value={contact.email}
+              placeholder="eMail"
+              required
+            ></input>
+            <input
+              type="text"
+              name="address"
+              value={contact.address}
+              placeholder="Address"
+              required
+            ></input>
+            <input
+              type="text"
+              name="phone"
+              value={contact.phone}
+              placeholder="Phone Number"
+              required
+            ></input>
+            <input
+              type="submit"
+              className="btn btn-success"
+              value="Add New Contact"
+              required
+            ></input>
+          </form>
+          <Link to="/">
+            <button className="btn btn-success" role="button" id="home">
+              Back to Home
+            </button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
 };
-
